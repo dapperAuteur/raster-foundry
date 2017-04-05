f3db14fa106814d1c871498d5380b0231c16c050
Colin Woodbury <colingw@gmail.com>
2017-04-05 08:08:28 -0700

    (aoi) Name primary key correctly in `aois_to_projects`

--- app-backend/database/src/main/scala/com/azavea/rf/database/tables/AoisToProjects.scala
+++ app-backend/database/src/main/scala/com/azavea/rf/database/tables/AoisToProjects.scala
@@ -15,7 +15,7 @@ class AoisToProjects(_tableTag: Tag) extends Table[AoiToProject](_tableTag, "aoi
   val aoiId: Rep[UUID] = column[UUID]("aoi_id", O.PrimaryKey)
   val projectId: Rep[UUID] = column[UUID]("project_id", O.PrimaryKey)
 
-  val pk = primaryKey("scenes_to_projects_pkey", (aoiId, projectId))
+  val pk = primaryKey("aois_to_projects_pkey", (aoiId, projectId))
 
   lazy val projectsFk = foreignKey("aoi_to_projects_project_id_fkey", projectId, Projects)(r => r.id, onUpdate=ForeignKeyAction.NoAction, onDelete=ForeignKeyAction.Cascade)
   lazy val aoiFk = foreignKey("aoi_to_projects_aoi_id_fkey", aoiId, Scenes)(r => r.id, onUpdate=ForeignKeyAction.NoAction, onDelete=ForeignKeyAction.Cascade)
